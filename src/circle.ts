50167543951b35572143b64d2d3990ba839975d6
Allie Howe <howeisallie@gmail.com>
2024-09-07 12:07:18 +0100

    styling

--- src/circle.ts
+++ src/circle.ts
@@ -8,12 +8,10 @@ export class Circle {
 
   draw(p5: P5) {
     const maxSize = Math.sqrt(p5.width ** 2 + p5.height ** 2)
+
     const size = ((p5.millis()/10) + this.offset) % maxSize
-
     const offset = Math.sin(this.offset * p5.millis() / 2e6) * 2
-
     const opacity = p5.map(size, 0, maxSize, inverse ? 0 : 255, inverse ? 255 : 0)
-
 
     p5.stroke(STROKE_COL, opacity)
     p5.circle(0, 0, size + offset)
